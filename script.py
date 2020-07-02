a182ce73292a968135a95bb67de9e2581d2da838
william-dvlpr <william.dvlpr@gmail.com>
2020-07-01 22:25:42 -0300

    repeat

--- script.py
+++ script.py
@@ -7,10 +7,22 @@ char_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
              '1', '2', '3', '4', '5', '6', '7', '8', '9',
              '#', '$', '%', '&', '*', '_', '(', ')', '-']
 
-password = ''
 
-for i in range(10):
-    pos = randint(0, len(char_list)-1)
-    password = password + char_list[pos]
+while True:
+    password = ''
+    for i in range(10):
+        pos = randint(0, len(char_list)-1)
+        password = password + char_list[pos]
 
-print(f'Password: \033[1;32m {password} \033[m')
+    print(f'Password: \033[1;32m{password}\033[m')
+    
+    reply = input('Deseja gerar outra senha? S/N: ').upper()
+
+    if reply == 'S':
+        continue
+    elif reply == 'N':
+        print('Ok, obrigado!')
+        break
+    else:
+        print('Resposta inválida :(')
+        break
